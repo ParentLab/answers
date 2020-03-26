028c6d8b5c31995c093df92226e3d1e4b68ab26d
Zhimin Chen <chenzhimin1231@gmail.com>
2020-03-26 13:09:46 -0700

    init

---
+++ src/components/my-components.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import MyComponent from './my-component';
+
+export default { title: 'MyComponent' };
+
+export const basic = () => <MyComponent />;
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import MyComponent from './components/my-component'
+
+export { MyComponent }
