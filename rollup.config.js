f85649a2d34ce444980d929fb1da3864f265c7f1
Zhimin Chen <chenzhimin1231@gmail.com>
2020-03-28 11:58:55 -0700

    rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import resolve from 'rollup-plugin-node-resolve'
+import json from '@rollup/plugin-json'
 
 import pkg from './package.json'
 
@@ -40,8 +41,18 @@ export default {
           'PropTypes',
           'createElement'
         ],
-        'node_modules/react-dom/index.js': ['render']
+        'node_modules/react-dom/index.js': ['render'],
+        'node_modules/react-is/index.js': [
+          'isElement',
+          'isValidElementType',
+          'ForwardRef',
+          'isFragment'
+        ],
+        'node_modules/lodash/lodash.js': [
+          'isEmpty'
+        ]
       }
-    })
+    }),
+    json()
   ]
 }
